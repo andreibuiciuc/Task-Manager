28862cd9d58062d69d7d977b2b15f6e77b975430
andreibuiciuc <abuiciuc33@gmail.com>
2022-02-28 14:19:24 +0200

    add task interface and mock tasks

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import {Task} from './Task'
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Gym Workout',
+        day: 'March 4th at 18:00',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Running Outdoors',
+        day: 'March 6th at 08:00',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Clothes Shopping',
+        day: 'March 7th at 19:00',
+        reminder: false
+    }
+]
